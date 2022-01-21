57a5a85c7ee0a6e531467564c738ab54945f1afb
Tyler Adam Martinez <tyleradammartinez@Paiges-MBP.attlocal.net>
2022-01-21 08:05:21 -0600

    feat: update topBar layout

--- public/js/topBar.js
+++ public/js/topBar.js
@@ -1,16 +1,105 @@
-const getGithubPeopleRequest = async (githubUser) => {
-    let GitHubResponse = await fetch(`https://api.github.com/users/${githubUser}`);
-    let GitHubResponseinJSON = await GitHubResponse.json();
-    return GitHubResponseinJSON;
+const delay = (seconds) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+           resolve(2); 
+        }, seconds);
+    });
 };
 
-const topBar = ((githubUser) => {
-    let GithubProfile = getGithubPeopleRequest(githubUser);
-    GithubProfile.then((GithubPeoplesData) => {
-        let public_repos = document.querySelector("#topBar h4");
-        let public_reposURL = document.createElement('a');
-        public_reposURL.setAttribute("href", `${GithubPeoplesData.html_url}?tab=repositories&type=public`);
-        public_reposURL.textContent = `Public Repos: ${GithubPeoplesData.public_repos}`;
-        public_repos.appendChild(public_reposURL);
-    });
-})("TylerAdamMartinez");
+const toggleSubtitles =  ( async() => {
+    let subtitleOne = document.querySelector('#cycletxt-1');
+    let subtitleTwo = document.querySelector('#cycletxt-2');
+    let subtitleThree = document.querySelector('#cycletxt-3');
+    let shortTimeDelay = 150;
+    let longTimeDelay = 5000;
+
+    while(true) {
+        subtitleOne.style.display = "block";
+        subtitleTwo.style.display = "none";
+        subtitleThree.style.display = "none";
+        subtitleOne.style.opacity = 1;
+
+        await delay(longTimeDelay);
+        subtitleOne.style.opacity = .875;
+
+        await delay(shortTimeDelay);
+        subtitleOne.style.opacity = .75;
+
+        await delay(shortTimeDelay);
+        subtitleOne.style.opacity = .625;
+
+        await delay(shortTimeDelay); 
+        subtitleOne.style.opacity = .5;
+
+        await delay(shortTimeDelay);
+        subtitleOne.style.opacity = .375;
+
+        await delay(shortTimeDelay);
+        subtitleOne.style.opacity = .25;
+
+        await delay(shortTimeDelay);
+        subtitleOne.style.opacity = .125;
+
+        await delay(shortTimeDelay); 
+        subtitleOne.style.opacity = 0;
+        subtitleOne.style.display = "none";
+        subtitleTwo.style.display = "block";
+
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = 1;
+
+        await delay(longTimeDelay);
+        subtitleTwo.style.opacity = .875;
+
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = .75;
+
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = .625;
+        
+        await delay(shortTimeDelay); 
+        subtitleTwo.style.opacity = .5;
+
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = .375;
+        
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = .25;
+        
+        await delay(shortTimeDelay);
+        subtitleTwo.style.opacity = .125;
+           
+        await delay(shortTimeDelay); 
+        subtitleTwo.style.opacity = 0;
+        subtitleTwo.style.display = "none";
+        subtitleThree.style.display = "block";
+
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = 1;
+        
+        await delay(longTimeDelay);
+        subtitleThree.style.opacity = .875;
+
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = .75;
+
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = .625;
+        
+        await delay(shortTimeDelay); 
+        subtitleThree.style.opacity = .5;
+
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = .375;
+        
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = .25;
+        
+        await delay(shortTimeDelay);
+        subtitleThree.style.opacity = .125;
+           
+        await delay(shortTimeDelay); 
+        subtitleThree.style.opacity = 0;
+    }
+
+})();
