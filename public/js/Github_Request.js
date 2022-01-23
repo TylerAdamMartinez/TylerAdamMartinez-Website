5f33e27e72068c2d7b49e472b5ec27a33d9f37b9
Tyler Adam Martinez <tyleradammartinez@Paiges-MBP.attlocal.net>
2022-01-23 00:40:36 -0600

    feat: github repos section will load locally if github api fails

--- public/js/Github_Request.js
+++ public/js/Github_Request.js
@@ -1,16 +1,39 @@
 export const GITHUB_LOGIN = "tyleradammartinez";
+import GitHubReposJSON from "../assets/files/github_repos.json" assert { type: "json" };
 
 export const getGithubReposRequest = async (githubUser) => {
-    let GitHubResponse = await fetch(`https://api.github.com/users/${githubUser}/repos`);
-    let GitHubResponseinJSON = await GitHubResponse.json();
+    let GitHubResponseinJSON;
+    await fetch(`https://api.github.com/users/${githubUser}/repos`)
+    .then((response) => {
+        if(response.ok) {
+            GitHubResponseinJSON = response.json();
+        }
+    })
+    .catch(() => {
+        console.log(`Github API failed to load ${githubUser}'s repos. Load ${githubUser}'s Github repos from local .json file`);
+        GitHubResponseinJSON = GitHubReposJSON;
+    });
     return GitHubResponseinJSON;
 };
 
+let repoCount = 0;
 export const getGithubRepoLanguageRequest = async (githubUser, githubRepo) => {
-    let languages = await fetch(`https://api.github.com/repos/${githubUser}/${githubRepo}/languages`)
-    let languagesJSON = languages.json();
+    let languagesJSON;
+    await fetch(`https://api.github.com/repos/${githubUser}/${githubRepo}/languages`)
+    .then((response) => {
+        if(response.ok) {
+            languagesJSON = response.json();
+        }
+    })
+    .catch(() => {
+        console.log(`Github API failed to load ${githubRepo} from user ${githubUser}`);
+        console.log(`Load ${githubUser}'s Github repos (${githubRepo}) from local .json file`);
+        let languageKey = GitHubReposJSON[repoCount]['language'];
+        languagesJSON =  {};
+        languagesJSON[languageKey] = repoCount;        
+        repoCount++;
+    });
     return languagesJSON;
 };
-
 
 export default getGithubReposRequest;
