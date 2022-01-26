75c25da582bae6d63044541de98d8196a801539e
Tyler Adam Martinez <tyleradammartinez@Paiges-MBP.attlocal.net>
2022-01-26 14:46:53 -0600

    fix(footer.js): characters after main url in copyright section are removed from user

--- public/js/footer.js
+++ public/js/footer.js
@@ -6,9 +6,24 @@ const displayCurrentYear = (() => {
 
 const displayCurrentUrl = (() => {
     let currentUrl = window.location.href;
-    currentUrl = currentUrl.replaceAll("/", "");
-    currentUrl = currentUrl.replace(/.+:/u, "");
-
+    
+    if (currentUrl.match(/\.wiki\/.+\S/) !== null) {
+        currentUrl = currentUrl.replace(/\.wiki\/.+\S/, "");
+        currentUrl += '.wiki';
+        currentUrl = currentUrl.replaceAll("/", "");
+        currentUrl = currentUrl.replace(/.+:/u, "");
+    } else if (currentUrl.match(/\.com\/.+\S/) !== null) {
+        currentUrl = currentUrl.replace(/\.com\/.+\S/, "");
+        currentUrl += '.com';
+        currentUrl = currentUrl.replaceAll("/", "");
+        currentUrl = currentUrl.replace(/.+:/u, "");
+    } else {
+        currentUrl = currentUrl.replace(/\/\n/, "");
+        currentUrl = currentUrl.replace(/(\/(?!.*\/)).+/, "");
+        currentUrl = currentUrl.replaceAll("/", "");
+        currentUrl = currentUrl.replace(/.+:/u, "");
+    }
+    
     let currentUrlElement = document.querySelector('#currentUrl');
     currentUrlElement.textContent = currentUrl;
 })();
